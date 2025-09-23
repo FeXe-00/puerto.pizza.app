a3ea97f5fd455d2705f130e058b45300602a95ac
Facundo Sosa Escalada <fsosaescalada@gmail.com>
2025-09-23 13:32:49 -0300

    fix: se sube archivo wrangler para el worker

--- components/Body.tsx
+++ components/Body.tsx
@@ -1,23 +1,19 @@
-"use client"
+"use client";
 
 import React from "react";
 import Instagram from "@mui/icons-material/Instagram";
 
 const Body = () => {
+  const instagramUrl =
+    "https://www.instagram.com/puertopizza_mza?igsh=OWFsM3pveThjOG02";
+
   return (
     <div className="flex flex-col justify-center gap-5">
       <button className="">Menu</button>
       <button className="">Take Away</button>
       <button className="">Pedidos Ya</button>
 
-      <div
-        onClick={() =>
-          window.open(
-            "https://www.instagram.com/puertopizza_mza?igsh=OWFsM3pveThjOG02"
-          )
-        }
-        className="w-full flex justify-center"
-      >
+      <div onClick={() => window.open(instagramUrl)} className="w-full flex justify-center">
         <Instagram />
       </div>
     </div>
