57418444fd24d8e7cccf71913d294e98d3a32949
Facundo Sosa Escalada <fsosaescalada@gmail.com>
2025-11-20 19:18:08 -0300

    feat: se agrega link a pedidos ya

--- components/Body.tsx
+++ components/Body.tsx
@@ -10,18 +10,20 @@ const Body = () => {
   const menuUrl =
     "https://drive.google.com/file/d/1bO65pcHNS-w20Q1X_LFVeZbGKTUm8IKA/view?usp=sharing";
 
+  const takeAwayUrl = "https://www.pedidosya.com.ar/restaurantes/maipu/puerto-pizza-3bfe16aa-6510-4147-b4c0-bac32a5bc818-menu";
+    
   const phoneNumber = "2615500624"; // Replace with the full phone number in international format, without '+'
   const whatsappLink = `https://wa.me/${phoneNumber}`;
 
   return (
     <div className="flex flex-col justify-center gap-5">
-      <button onClick={() => window.open(menuUrl)} className="">
+      <button disabled={!instagramUrl} onClick={() => window.open(menuUrl)} className="">
         Menu
       </button>
-      <button onClick={() => window.open(whatsappLink)} className="">
+      <button disabled={!menuUrl} onClick={() => window.open(whatsappLink)} className="">
         Take Away
       </button>
-      <button disabled={true}>Pedidos Ya</button>
+      <button disabled={!takeAwayUrl} onClick={() => window.open(takeAwayUrl)}>Pedidos Ya</button>
 
       <div
         onClick={() => window.open(instagramUrl)}
