7a89449bd581e26d177dbd34b43056777bbca601
Facundo Sosa Escalada <fsosaescalada@gmail.com>
2025-11-20 20:27:55 -0300

    feat: se agrega link del nuevo menu con promociones

--- components/Body.tsx
+++ components/Body.tsx
@@ -8,22 +8,34 @@ const Body = () => {
     "https://www.instagram.com/puertopizza_mza?igsh=OWFsM3pveThjOG02";
 
   const menuUrl =
-    "https://drive.google.com/file/d/1bO65pcHNS-w20Q1X_LFVeZbGKTUm8IKA/view?usp=sharing";
+    "https://drive.google.com/file/d/1-YTXWc96l2IH57SQC8pjr5CiH3Qsx6RQ/view?usp=sharing";
+  // "https://drive.google.com/file/d/1bO65pcHNS-w20Q1X_LFVeZbGKTUm8IKA/view?usp=sharing";
 
-  const takeAwayUrl = "https://www.pedidosya.com.ar/restaurantes/maipu/puerto-pizza-3bfe16aa-6510-4147-b4c0-bac32a5bc818-menu";
-    
+  const takeAwayUrl =
+    "https://www.pedidosya.com.ar/restaurantes/maipu/puerto-pizza-3bfe16aa-6510-4147-b4c0-bac32a5bc818-menu";
+
   const phoneNumber = "2615500624"; // Replace with the full phone number in international format, without '+'
   const whatsappLink = `https://wa.me/${phoneNumber}`;
 
   return (
     <div className="flex flex-col justify-center gap-5">
-      <button disabled={!instagramUrl} onClick={() => window.open(menuUrl)} className="">
+      <button
+        disabled={!instagramUrl}
+        onClick={() => window.open(menuUrl)}
+        className=""
+      >
         Menu
       </button>
-      <button disabled={!menuUrl} onClick={() => window.open(whatsappLink)} className="">
+      <button
+        disabled={!menuUrl}
+        onClick={() => window.open(whatsappLink)}
+        className=""
+      >
         Take Away
       </button>
-      <button disabled={!takeAwayUrl} onClick={() => window.open(takeAwayUrl)}>Pedidos Ya</button>
+      <button disabled={!takeAwayUrl} onClick={() => window.open(takeAwayUrl)}>
+        Pedidos Ya
+      </button>
 
       <div
         onClick={() => window.open(instagramUrl)}
